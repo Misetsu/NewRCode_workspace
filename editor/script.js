8bd6ebc0783d005b504ed2a39a585758d7c76980
NakitaMak <kd1334035@st.kobedenshi.ac.jp>
2023-12-07 14:56:15 +0900

    リサイズ

--- editor/script.js
+++ editor/script.js
@@ -208,7 +208,7 @@ document.getElementById("input-files").onchange = function(e) {
   }
   
   
-  document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', function (e) {
     //Backspaceキーが押されたときのイベントを処理
     //Backspaceが押されたとき
     if (e.key === 'Backspace') {
@@ -219,20 +219,94 @@ document.getElementById("input-files").onchange = function(e) {
           activeObject.text = '';
           canvas.renderAll();
       } elseif (activeObject) 
-   }
-  
-  // Delete キーが押されたときのイベントを処理
-  if (e.key === 'Delete') {
-    // 選択されているオブジェクトを取得
-    var activeObjects = canvas.getActiveObjects();
-    // 選択されているオブジェクトが存在する場合
-    if (activeObjects.length > 0) {
-      // オブジェクトを削除
-      canvas.remove(...activeObjects);
-      // 描画を更新
-      canvas.discardActiveObject().renderAll();
     }
-  }
-  });
+  
+    // Delete キーが押されたときのイベントを処理
+    if (e.key === 'Delete') {
+        // 選択されているオブジェクトを取得
+        var activeObjects = canvas.getActiveObjects();
+        // 選択されているオブジェクトが存在する場合
+        if (activeObjects.length > 0) {
+                // オブジェクトを削除
+            canvas.remove(...activeObjects);
+            // 描画を更新
+            canvas.discardActiveObject().renderAll();
+        }
+    }
+});
+
+function autoA3() {
+    if (document.getElementById("Height").value == "4093") {
+        document.getElementById("Height").value = "5787";
+        document.getElementById("Width").value = "4093";
+    } else {
+        document.getElementById("Height").value = "4093";
+        document.getElementById("Width").value = "5787";
+    }
+}
+
+function autoA4() {
+    if (document.getElementById("Height").value == "2894") {
+        document.getElementById("Height").value = "4093";
+        document.getElementById("Width").value = "2894";
+    } else {
+        document.getElementById("Height").value = "2894";
+        document.getElementById("Width").value = "4093";
+    }
+}
+
+function autoA5() {
+    if (document.getElementById("Height").value == "2039") {
+        document.getElementById("Height").value = "2894";
+        document.getElementById("Width").value = "2039";
+    } else {
+        document.getElementById("Height").value = "2039";
+        document.getElementById("Width").value = "2894";
+    }
+}
+
+function autoB4() {
+    if (document.getElementById("Height").value == "3541") {
+        document.getElementById("Height").value = "5016";
+        document.getElementById("Width").value = "3541";
+    } else {
+        document.getElementById("Height").value = "3541";
+        document.getElementById("Width").value = "5016";
+    }
+}
+function autoB5() {
+    if (document.getElementById("Height").value == "2508") {
+        document.getElementById("Height").value = "3541";
+        document.getElementById("Width").value = "2508";
+    } else {
+        document.getElementById("Height").value = "2508";
+        document.getElementById("Width").value = "3541";
+    }
+}
+function autoCard() {
+    if (document.getElementById("Height").value == "1378") {
+        document.getElementById("Height").value = "2039";
+        document.getElementById("Width").value = "1378";
+    } else {
+        document.getElementById("Height").value = "1378";
+        document.getElementById("Width").value = "2039";
+    }
+}
+
+function reSize() {
+    document.getElementById("Height").value;
+    document.getElementById("Width").value;
+    let canvas1 = document.querySelector('#canvas');
+    canvas1.height = parseInt(document.getElementById("Height").value);
+    canvas1.width = parseInt(document.getElementById("Width").value);
+    canvas1.style.height = parseInt(document.getElementById("Height").value) + "px";
+    canvas1.style.width = parseInt(document.getElementById("Width").value) + "px";
+
+    let canvas2 = document.querySelector('.upper-canvas');
+    canvas2.height = parseInt(document.getElementById("Height").value);
+    canvas2.width = parseInt(document.getElementById("Width").value);
+    canvas2.style.height = parseInt(document.getElementById("Height").value) + "px";
+    canvas2.style.width = parseInt(document.getElementById("Width").value) + "px";
+}
 
 resetSettings();

de36013ede54f54be663163a7c4ef95aedec2971
Misetsu <nakitamak@gmail.com>
2023-12-20 13:22:05 +0900

    flask^^

--- form.py
+++ form.py
@@ -1,3 +1,21 @@
+from flask import Flask, request
+from datetime import datetime
+import os
+
+UPLOAD_FOLDER = './uploads'
+ICON_FOLDER = './icon'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+ICON = ('amazon.png', 'chrome.png', 'discord.png', 'facebook.png', 'google.png', 
+        'instagram.png', 'line.png', 'snapchat.png', 'tiktok.png', 'twitch.png', 
+        'twitter.png', 'wechat.png', 'weibo.png', 'whatsapp.png', 'youtube.png')
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ICON_FOLDER'] = ICON_FOLDER
+
+def allwed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/qrform', methods=["GET", "POST"])
 def form():
     if request.method == 'GET':
@@ -5,12 +23,21 @@ def form():
     if request.method == 'POST':
         image1 = request.file["backImage"]
         if image1 and allwed_file(image1.filename):
+            dt = datetime.utcnow().strftime('%Y%m%d%H%M%S%f')[:-3]
             filename1 = secure_filename(image1.filename)
+            filename1 = dt + filename1
             image1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
+            
+        image2 = request.form["iconImage"]
+        if image2 == "":
+            filepath2 = ""
+        else:
+            filepath2 = os.path.join(app.config['ICON'], ICON[int(image2)])
         
+        filepath1 = os.path.join(app.config['UPLOAD_FOLDER'], dt, filename1)
         url = request.form["urlText"]
         color = request.form["qrColor"]
         colorCode = request.form["colorCode"]
         alpha = request.form["qrAlpha"]
     
-    print(filename1, url, color, colorCode, alpha)
+    return(f'{filepath1}, {filepath2}, {filename1}, {url}, {color}, {colorCode}, {alpha}')

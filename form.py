30f610648bc0b6f1ffb88db2b5107a978a639fd9
NakitaMak <kd1334035@st.kobedenshi.ac.jp>
2024-01-03 22:37:27 +0900

    記憶を頼りに書いたFlask(デバッグ必須)

--- form.py
+++ form.py
@@ -1,6 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
+from PIL import Image
+from io import BytesIO
 from datetime import datetime
 import os
+import base64
 
 UPLOAD_FOLDER = './uploads'
 ICON_FOLDER = './icon'
@@ -24,7 +27,6 @@ def form():
         image1 = request.file["backImage"]
         if image1 and allwed_file(image1.filename):
             dt = datetime.utcnow().strftime('%Y%m%d%H%M%S%f')[:-3]
-            filename1 = secure_filename(image1.filename)
             filename1 = dt + filename1
             image1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
             
@@ -41,3 +43,33 @@ def form():
         alpha = request.form["qrAlpha"]
     
     return(f'{filepath1}, {filepath2}, {filename1}, {url}, {color}, {colorCode}, {alpha}')
+
+@app.route('/qrform', methods=["GET", "POST"])
+def form():
+    if request.method == 'POST':
+        data = request.get_json()
+        
+        filename1 = data[1]["filename"]
+        base64Img = data[0]["backimageURL"]
+        code = base64.b64decode(base64Img.split(',')[1])
+        image_decoded = Image.open(BytesIO(code))
+        image_decoded.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
+        
+        if data[2]["iconCode"] == "":
+            filepath2 = ""
+        else:
+            filepath2 = os.path.join(app.config['ICON'], ICON[int(data[2]["iconCode"])])
+            
+        filepath1 = os.path.join(app.config['UPLOAD_FOLDER'], filename1)
+        url = data[3]["url"]
+        color = data[4]["color"]
+        colorCode = data[5]["colorCode"]
+        alpha = data[6]["alpha"]
+        
+        qrpath = pre(filepath1, filepath2, filename1, url, color, colorCode, alpha)
+        
+        with open(qrpath, "rb") as f:
+            image_binary = f.read()    
+        image = base64.b64encode(image_binary).decode("utf-8")
+        
+        return jsonify({'status': True, 'image': image})
